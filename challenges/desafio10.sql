c87a6771f2c0ad2f9d7469a829ca7fd222e85c78
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-03-06 18:13:20 -0300

    desafio 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,24 @@
--- Descomente e altere as linhas abaixo:
+CREATE TABLE cancoes_favoritas(
+  favoritas_id INT NOT NULL AUTO_INCREMENT,
+  usuario_id INT NOT NULL,
+  cancoes_id INT NOT NULL,
+  PRIMARY KEY (favoritas_id, usuario_id, cancoes_id),
+  FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id),
+  FOREIGN KEY (cancoes_id) REFERENCES cancoes(cancoes_id)
+);
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
-
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO cancoes_favoritas (cancoes_id, usuario_id)
+VALUES
+  ('10', '1'),
+  ('4', '1'),
+  ('8', '1'),
+  ('2', '2'),
+  ('1', '3'),
+  ('10', '3'),
+  ('5', '4'),
+  ('2', '4'),
+  ('8', '5'),
+  ('9', '5'),
+  ('2', '8'),
+  ('5', '9'),
+  ('10', '10');
